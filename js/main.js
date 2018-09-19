f12609cdb3ac2c19e2ba7fb5bb2ed0ea6f6ef28b
nriddiford <nickriddiford@me.com>
2018-09-19 18:24:04 +0200

    Improve responsive features

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
     $('.header').height($(window).height());
 });
 
+// Get height of screen for container sizing
+$(document).ready(function(){
+    $('.container').height($(window).height());
+});
+
 
 // Scroll to id
 $(".navbar a").click(function(){
@@ -19,28 +24,3 @@ $(".header button").click(function(){
     },1000)
 
 });
-
-
-
-// // navbar collapse
-// $(document).ready(function () {
-//
-//     $(window).on('scroll', function () {
-//         if ($(window).scrollTop() >= 25) { // use any value lower than the navbar height, [20] is an example
-//
-//             $('.navbar').css({ // reducing the vertical padding from 25px to 10px
-//                 'padding-top': '10px',
-//                 'padding-bottom': '10px'
-//             });
-//
-//         } else {
-//
-//             $('.navbar').css({ // reset the vertical padding to its initial value [25px]
-//                 'padding-top': '25px',
-//                 'padding-bottom': '25px'
-//             });
-//
-//         }
-//     });
-//
-// });
