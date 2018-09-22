151e4c45ee039426e3d3bc8a9ee76e8957bd7dc4
nriddiford <nickriddiford@me.com>
2018-09-22 12:10:27 +0200

    Adjust offset in scroll js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 // Get height of screen for header
-$(document).ready(function(){
+$(document).ready(function () {
     $('.header').height($(window).height());
 });
 
-// Get height of screen for container sizing
-$(document).ready(function(){
-    $('.container').height($(window).height());
-});
+// // Get height of screen for container sizing
+// $(document).ready(function(){
+//     $('.container').height($(window).height());
+// });
 
 
 // Scroll to id
-$(".navbar a").click(function(){
+$(".navbar a").click(function () {
     $("body,html").animate({
-        scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
+        scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height()
+    }, 1000)
 
 });
 
 // Header button -> scroll down
-$(".header button").click(function(){
+$(".header button").click(function () {
     $("body,html").animate({
-        scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000)
 
 });
