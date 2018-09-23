27ae0f2cbd200faaf94acba08bbb557554ae0087
nriddiford <nickriddiford@me.com>
2018-09-23 13:10:08 +0200

    Add js scroll animation

--- js/main.js
+++ js/main.js
@@ -24,3 +24,30 @@ $(".header button").click(function () {
     }, 1000)
 
 });
+
+
+
+
+// GENERAL SETTING
+
+
+// // Custom Settings
+// sr.reveal('.foo-1', { duration: 200 });
+//
+// sr.reveal('.foo-2', {
+//     origin: 'right',
+//     duration: 2000
+// });
+//
+// sr.reveal('.foo-3', {
+//     rotate: { x: 100, y: 100, z: 0 },
+//     duration: 1000
+// });
+//
+// sr.reveal('.foo-4', {
+//     viewFactor: 0.5
+// });
+//
+// sr.reveal('.foo-5', {
+//     duration: 200
+// });
