09a860df6a1ad9b772fadc65b1279822a926ef14
nriddiford <nickriddiford@me.com>
2018-11-18 11:12:58 +0100

    Final buttons and paralax

--- js/main.js
+++ js/main.js
@@ -3,27 +3,24 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 });
 
-// Scroll to id from nav items
-$(".navbar a").click(function () {
-    $("body,html").animate({
-        scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 10
-    }, 1000)
 
+// Generic scrolling animation
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            var element = this.hash;
+            var nav_offset = $('.navbar').height() + 15
+            $('html, body').animate({
+                scrollTop: $(element).offset().top-nav_offset}, 1000, function(){
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // THis was causing problems 13.11.18
+                // window.location.hash = element;
+            });
+        }
+    });
 });
 
-// Header button -> scroll down
-$(".header button").click(function () {
-    $("body,html").animate({
-        scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 10
-    }, 1000)
-
-});
-
-// // Reveal text on scroll through
-// window.sr = ScrollReveal({
-//     reset: true,
-//     easing: 'ease-in'
-// });
-// sr.reveal('.reveal', { opacity: 0.1, duration:600 });
 ScrollReveal().reveal('.reveal', {reset: true})
-// ScrollReveal().reveal('.reveal', { reset: true, easing: 'ease-in', opacity: 0.1, duration:600 });
